5e5560dfcf1205390dc5fb36dc7b6836e46b066c
Stefan Bjornander <stefan.bjornander@outlook.com>
2022-02-15 15:48:46 +0100

    Backup

--- g.bat
+++ g.bat
@@ -4,75 +4,64 @@
 
 @C:
 @cd "C:\Users\stefa\Documents\A A C_Compiler_CSharp_8"
-@rem echo "# CCompiler8" > README.md
-@rem git init
 @git add .
 @git commit -m "Backup"
 @rem git branch -M master
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/CCompiler8.git
 @git push -u origin master
-@rem pause
 
-@C:
 @cd "C:\Users\stefa\Documents\A A C_Compiler_CSharp_10"
-@rem echo "# CCompiler10" > README.md
-@rem git init
 @git add .
 @git commit -m "Backup"
-@rem git branch -M master
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/CCompiler10.git
 @git push -u origin master
-@rem pause
 
-@cd "C:\Users\Stefa\Documents\A A A C_Compiler_Assembler - CSharp 0 Assembly"
+@cd "C:\Users\stefa\Documents\A A C_Compiler_Assembler - CSharp 0 Assembly"
+@git add .
+@git commit -m "Backup"
+@git remote remove origin
+@git remote add origin https://github.com/StefanBjornander/CCompiler_Assembly.git
+@git push -u origin master
+
+@cd "C:\Users\stefa\Documents\vagrant\homestead\code"
+@git init
+@git add .
+@git commit -m "Backup"
+@git remote remove origin
+@git remote add origin https://github.com/StefanBjornander/Code.git
+@git push -u origin master
+
+@cd "C:\Users\Stefa\Documents\GenerateAssembly"
 @git init
 @git add .
 @git commit -m "Backup"
 @git branch -M master
 @git remote remove origin
-@git remote add origin https://github.com/StefanBjornander/CCompiler_Assembly.git
+@git remote add origin https://github.com/StefanBjornander/GenerateAssembly.git
 @git push -u origin master
-@rem pause
-
-@cd "C:\Users\Stefa\Documents\vagrant\homestead\code"
-@git add .
-@git commit -m "Backup"
-@git branch -M master
-@git remote remove origin
-@git remote add origin https://github.com/StefanBjornander/Code.git
-@git push -u origin master
-@rem pause
-
-@cd "C:\Users\Stefa\Documents\GenerateAssembler"
-@git add .
-@git commit -m "Backup"
-@git branch -M master
-@git remote remove origin
-@git remote add origin https://github.com/StefanBjornander/GenerateAssembler.git
-@git push -u origin master
-@rem pause
 
 @cd "C:\Users\stefa\Documents\ObjectCodeTableGeneratorCSharp"
+@git init
 @git add .
 @git commit -m "Backup"
 @git branch -M master
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/ObjectCodeTableGeneratorCSharp.git
 @git push -u origin master
-@rem pause
 
 @cd "C:\Users\stefa\Documents\ObjectCodeTableGeneratorJava"
+@git init
 @git add .
 @git commit -m "Backup"
 @git branch -M master
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/ObjectCodeTableGeneratorJava.git
 @git push -u origin master
-@rem pause
 
 @cd "C:\Users\stefa\Documents\Languages"
+@git init
 @git add .
 @git commit -m "Backup"
 @git branch -M master
