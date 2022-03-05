53f9fbca7212e457c5c5f461af12367d1fa31245
Stefan Bjornander <stefan.bjornander@outlook.com>
2022-03-05 21:07:10 +0100

    Backup

--- g.bat
+++ g.bat
@@ -92,10 +92,10 @@
 @git remote remove origin
 @git remote add origin https://github.com/StefanBjornander/Books.git
 @git push origin master
-@pause
 
 @echo 11
 @cd "C:\xampp\htdocs"
+@git init
 @git add .
 @git commit -m "Backup"
 @git branch -M master
